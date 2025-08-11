d91aee5d7342ed64ea605d32d49955289c95e503
Padu76 <andrea.padoan@gmail.com>
2025-08-11 22:24:16 +0200

    Add files via upload

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,1 +1,25 @@
+'use client'
 
+import { ReactNode } from 'react'
+
+interface ProvidersProps {
+  children: ReactNode
+}
+
+export function Providers({ children }: ProvidersProps) {
+  // Qui aggiungeremo i vari provider man mano che sviluppiamo
+  // Per ora è un wrapper semplice che prepara la struttura
+  
+  return (
+    <>
+      {/* Future: SupabaseProvider */}
+      {/* Future: AuthProvider */}
+      {/* Future: ThemeProvider */}
+      {/* Future: StoreProvider (Zustand) */}
+      {/* Future: ToastProvider */}
+      {/* Future: AnalyticsProvider */}
+      
+      {children}
+    </>
+  )
+}
